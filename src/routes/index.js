c5e801b41125ca983d35247685a95437b1ab1790
melihhmerall <128594380+melihhmerall@users.noreply.github.com>
2024-05-05 12:24:30 +0300

    login_and_fixes
    Login-logout page and some fixes

--- src/routes/index.js
+++ src/routes/index.js
@@ -1,5 +1,17 @@
+const users = require("./users");
 const login = require("./login");
+const events = require("./events");
+const podcasts =require("./podcasts");
+const videos = require("./videos");
+const newsletter = require("./newsletter");
+const logout = require("./logout");
 
 module.exports = (app) => {
+  app.use("/users", users);
   app.use("/login", login);
+  app.use("/events", events);
+  app.use("/podcasts", podcasts);
+  app.use("/videos", videos);
+  app.use("/newsletter", newsletter);
+  app.use("/logout", logout);
 };
