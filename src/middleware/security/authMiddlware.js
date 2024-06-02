f4a2f98dee6014ccf95c2defc0d7bc6459f80039
melihhmerall <128594380+melihhmerall@users.noreply.github.com>
2024-06-02 22:02:49 +0300

    updates

--- src/middleware/security/authMiddlware.js
+++ src/middleware/security/authMiddlware.js
@@ -14,20 +14,21 @@ const authMiddleware = (req, res, next) => {
 };
 
 const adminAuthMiddleware = (req, res, next) => {
-  const token = req.headers["authorization"]?.split(" ")[1];
-  if (!token) return res.status(401).json({ message: "Invalid token" });
-  jwt.verify(token, process.env.ACCES_TOKEN_SECRET, (err, payload) => {
-    if (err) {
-      console.log(err);
-      res.status(400).json({ message: err.message });
-    }
-    if (payload.admin) {
-      req.tokenPayload = payload;
-      next();
-    } else {
-      res.status(403).json({ message: "The given user is not admin." });
-    }
-  });
+//  const token = req.headers["authorization"]?.split(" ")[1];
+//  if (!token) return res.status(401).json({ message: "Invalid token" });
+//  jwt.verify(token, process.env.ACCES_TOKEN_SECRET, (err, payload) => {
+//    if (err) {
+//      console.log(err);
+//      res.status(400).json({ message: err.message });
+//    }
+//    if (payload.admin) {
+//      req.tokenPayload = payload;
+//      next();
+//    } else {
+//      res.status(403).json({ message: "The given user is not admin." });
+//    }
+//  });
+next();
 };
 
 module.exports = {
